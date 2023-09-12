8932973e4949e77666ce8fd48c09053409a061bd
Maxime <maxime.marechalmccoy@gmail.com>
2023-09-12 15:15:37 -0700

    fteched db tweets and tiem formatting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,32 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
 */
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 const createTweetElement = (tweetObj) => {
   return `<article class="tweet-container">
     <header class="tweet-header">
@@ -41,7 +15,7 @@ const createTweetElement = (tweetObj) => {
     <p>${tweetObj.content.text}</p>
     <!-- <br> -->
     <footer>
-      <div>${(new Date() - tweetObj.created_at).toLocaleString()} days ago</div>
+    <div>${timeago.format(tweetObj.created_at)}</div>
       <div class="icons">
         <i class="fa-solid fa-flag"></i>
         <i class="fa-solid fa-retweet"></i>
@@ -65,9 +39,13 @@ const renderTweets = function(tweetsDataArray) {
 };
 
 $(document).ready(function() {
-
-  // Test / driver code (temporary). Eventually will get this from the server.
-  // $(".tweet-container").after($tweet);
-  renderTweets(tweetData);
+  const loadTweets = function() {
+    $.ajax("/tweets", { method: 'GET' })
+      .then(tweets => {
+        console.log('tweets from get tweets', tweets);
+        renderTweets(tweets);
+      });
+  };
+  loadTweets();
 
 });
